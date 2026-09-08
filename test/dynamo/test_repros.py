12c6c46fca398f61a42d48086b6f4f65e3d90f73
zengxian <xiangdong.zeng@intel.com>
2026-09-08 14:49:00 +0800

    test/dynamo/test_repros.py

--- test/dynamo/test_repros.py
+++ test/dynamo/test_repros.py
@@ -85,7 +85,6 @@ from torch.testing._internal.common_utils import (
     skipIfRocm,
     skipIfWindows,
     skipIfXpu,
-    TEST_ACCELERATOR,
     TEST_WITH_ROCM,
     xfailIfS390X,
 )
@@ -8884,6 +8883,7 @@ class ReproTestsDevice(torch._dynamo.test_case.TestCase):
                 if isinstance(ev, torch.Tensor):
                     self.assertEqual(ev.device.type, "cpu")
 
+    @onlyAccelerator
     @torch._dynamo.config.patch(capture_scalar_outputs=False)
     def test_aot_backward_context_reentry_after_graph_break(self, device):
         def fn(x, y, scalar):
@@ -8898,15 +8898,14 @@ class ReproTestsDevice(torch._dynamo.test_case.TestCase):
         y = torch.randn(8, device=device, requires_grad=True)
         scalar = torch.randn((), device=device)
 
-        before_break, after_break = torch.compile(fn, backend="aot_eager")(
-            x, y, scalar
-        )
+        before_break, after_break = torch.compile(fn, backend="aot_eager")(x, y, scalar)
         loss = before_break.sum().to(device) + after_break.sum()
         loss.backward()
 
         self.assertEqual(x.grad, torch.ones_like(x))
         self.assertEqual(y.grad, -y.detach().sin())
 
+    @onlyAccelerator
     def test_cuda_sync(self, device):
         def fn(x):
             y = x + 1
@@ -8919,6 +8918,7 @@ class ReproTestsDevice(torch._dynamo.test_case.TestCase):
         self.assertEqual(fn(x), opt_fn(x))
         self.assertEqual(cnt.frame_count, 1)
 
+    @onlyAccelerator
     def test_device_context_matmul_avoids_native_bmm_router_graph_break(self, device):
         torch._dynamo.utils.counters.clear()
 
@@ -9334,7 +9334,6 @@ class ReproTestsDevice(torch._dynamo.test_case.TestCase):
         mem_after = torch.accelerator.memory_allocated()
         self.assertEqual(mem_before, mem_after)
 
-    @skipIfXpu(msg="https://github.com/intel/torch-xpu-ops/issues/3835")
     def test_sdpa_dynamic_shapes(self, device):
         def f(x, s0, s1, s2):
             q = x.view(2, s0, s2, s0)
